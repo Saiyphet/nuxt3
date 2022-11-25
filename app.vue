5588cbc762293077dd00a826847f4bce71b2cde9
Saiyphet <101562973+Saiyphet@users.noreply.github.com>
2022-11-26 04:14:52 +0700

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,152 @@
+<style>
+@import './Style.css';
+</style>
+<script>
+export default {
+  head() {
+    return {
+      bodyAttrs: {
+        class: 'reset-body'
+      },
+    }
+  }
+}
+</script>
+<template>
+  <div class="nav">
+    <div class="nav1">
+      <div class="paddingleft">
+        <div>+856 (021) 755 222, 021 755 333</div>
+      </div>
+      <div class="paddingleft1">
+        <div>Monday – Friday 10 AM – 8 PM</div>
+      </div>
+    </div>
+  </div>
+  <div class="underline"></div>
+  <div class="nav2">
+    <div class="nav3">
+      <div class="imgpadding">
+        <img src='./Img/newlogosit.png' />
+      </div>
+      <div class="text">
+        <div class="textpadding">ໜ້າຫຼັກ</div>
+        <div class="textpadding">ກ່ຽວກັບ</div>
+        <div class="textpadding">ພາກວິຊາ ແລະ ຫຼັກສູດ</div>
+        <div class="textpadding">ການລົງທະບຽນ</div>
+        <div class="textpadding">ນັກສຶກສາ</div>
+        <div class="textpadding">ການເຄື່ອນໄຫວ</div>
+        <div class="textpadding">ຕິດຕໍ່</div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <img src='./Img/slide-xl.jpg' class="poster" />
+  </div>
+  <div class="underline1">
+    <div class="nav4">
+      <div class="nav5">
+        <div>
+          <div class="textbtn">ທຶນການສຶກສາ</div>
+        </div>
+        <div class="textpadding">
+          <div class="textbtn">ຫລັກສູດຊັ້ນສູງ</div>
+        </div>
+        <div class="textpadding">
+          <div class="textbtn">ຫລັກສູດປະລິນຍາຕີ</div>
+        </div>
+        <div class="textpadding">
+          <div class="textbtn">ຫລັກສູດໄລຍະສັ້ນ</div>
+        </div>
+      </div>
+    </div>
+    <div class="underline2">
+      <div class="nav4">
+        <div class="nav5">
+          <div>
+            <div class="textbtn1">ດາວໂຫລດຟອມສະໝັກຮຽນ</div>
+          </div>
+          <div class="textpadding">
+            <div class="textbtn1">ລົງທະບຽນອອນລາຍ</div>
+          </div>
+          <div class="textpadding">
+            <div class="textbtn1">International Program</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="underline3">
+    <div class="nav4">
+      <div class="nav5">
+        <div class="underline4"></div>
+      </div>
+    </div>
+  </div>
+  <div class="nav4">
+    <div class="nav6">
+      <div class="txt">ກ່ຽວກັບສະຖາບັນ</div>
+    </div>
+  </div>
+  <div class="underline3">
+    <div class="nav">
+      <div class="navm1">
+        <div class="div1">
+          <div class="div7">
+            <div class="div2">
+              <div class="div3">
+                <div class="div4">
+                  <div class="div5"></div>
+                  <div class="div6">
+                    <div class="div5"></div>
+                  </div>
+                  <div class="div6">
+                    <div class="div5"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          ການຮັບຮອງຈາກອົງການຈັດຕັ້ງລັດ
+        </div>
+        <div class="div1">
+          <div class="div8">
+            <div class="div2">
+              <div class="div3">
+                <div class="div4">
+                  <div class="div5"></div>
+                  <div class="div6">
+                    <div class="div5"></div>
+                  </div>
+                  <div class="div6">
+                    <div class="div5"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          ສັນຍາລັກ
+        </div>
+        <div class="div1">
+          <div class="div8">
+            <div class="div2">
+              <div class="div3">
+                <div class="div4">
+                  <div class="div5"></div>
+                  <div class="div6">
+                    <div class="div5"></div>
+                  </div>
+                  <div class="div6">
+                    <div class="div5"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          ການຮ່ວມມື
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
